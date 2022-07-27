42625be5578ef86c59dd61ceb2f86fa9af40538a
Erika Rivera <rivera.erika07@gmail.com>
2022-07-27 15:57:43 -0400

    Magic 8 Ball express application created.

--- homework/server.js
+++ homework/server.js
@@ -3,6 +3,8 @@ require ('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3003;
 
+// const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
 const fs = require('fs') // this engine requires the fs module like we did Saturday
 app.engine('hypatia', (filePath, options, callback) => { // define the view engine called hypatia
   fs.readFile(filePath, (err, content) => {
@@ -30,6 +32,14 @@ app.get ('/greeting/:name',(req,res) => {
     let tip = req.params.total * (req.params.percentage/100)
     res.render('template', { title: 'TIP', message: 'your subtotal is $' + req.params.total + ' tip will be $' + tip})
   })
+
+  const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+  app.get ('/magic/:question',(req,res) => {
+   const response = responses[Math.floor(Math.random() * responses.length)]
+      
+    res.render('magic', { title: 'Magic', message: response})
+})
 
 
 app.listen(port,() => {
