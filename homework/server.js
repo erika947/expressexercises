71337db806571c071f4f95575fd81080805f99ed
Erika Rivera <rivera.erika07@gmail.com>
2022-07-28 21:27:01 -0400

    clean up

--- homework/server.js
+++ homework/server.js
@@ -3,8 +3,6 @@ require ('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3003;
 
-const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
-
 const fs = require('fs') // this engine requires the fs module like we did Saturday
 app.engine('hypatia', (filePath, options, callback) => { // define the view engine called hypatia
   fs.readFile(filePath, (err, content) => {
@@ -16,10 +14,23 @@ app.engine('hypatia', (filePath, options, callback) => { // define the view engi
     return callback(null, rendered)
   })
 })
+
 app.set('views', './views') // specify the views directory
 app.set('view engine', 'hypatia') // register the hypatia view engine
 
-app.get('/greeting',(req,res) => {
+const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+//Take One Down and Pass it Around
+app.get("/", (request, response) => {
+  response.send("99 Bottles of beer on the wall");
+});
+
+app.get("/:number_of_bottles", (req,res) => {
+  res.send(req.params.number_of_bottles + ' bottles of beer on the wall')
+})//The number appears with the sentence. next step is to connect it to a template so a link can be added.
+
+//Greetings
+app.get('/greeting/hello_stranger',(req,res) => {
     res.send('Hello, Stranger')
 })
 
@@ -27,13 +38,14 @@ app.get ('/greeting/:name',(req,res) => {
     res.send("Hello " + req.params.name)
 })
 
+//Tip Calcuator
 
   app.get('/tip/:total/:percentage', (req, res) => {
     let tip = req.params.total * (req.params.percentage/100)
     res.render('template', { title: 'TIP', message: 'your subtotal is $' + req.params.total + ' tip will be $' + tip})
   })
 
-
+//Magic 8 Ball
   app.get ('/magic/:question',(req,res) => {
    const response = responses[Math.floor(Math.random() * responses.length)]
       
@@ -41,6 +53,6 @@ app.get ('/greeting/:name',(req,res) => {
 })
 
 
-app.listen(port,() => {
-    console.log('i am listening on port ', port)
-})
+app.listen(port, () => {
+  console.log(`*** Listening on http://localhost:${port} ***`)
+})  
